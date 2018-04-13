5be3d2e309806fe9e70ed9eb47f9ff2dcb08f2ef
Ashley Mannix <ashleymannix@live.com.au>
2018-04-13 14:21:11 +1000

    Deprecate listenUris in favour of baseUri (#1)

--- src/Seq.Apps/Apps/Host.cs
+++ src/Seq.Apps/Apps/Host.cs
@@ -1,4 +1,6 @@
-﻿namespace Seq.Apps
+﻿using System;
+
+namespace Seq.Apps
 {
     /// <summary>
     /// Represents the Seq instance running the app.
@@ -10,10 +12,32 @@
         /// </summary>
         /// <param name="instanceName">The name of the Seq instance, or null.</param>
         /// <param name="listenUris">The URIs at which the server is listening.</param>
+        [Obsolete("Use the Host(baseUri, instanceName) constructor.")]
         public Host(string[] listenUris, string instanceName)
         {
+            if (listenUris == null || listenUris.Length == 0)
+            {
+                throw new ArgumentException("At least 1 listen uri must be given.", nameof(listenUris));
+            }
+
             InstanceName = instanceName;
             ListenUris = listenUris;
+            BaseUri = listenUris[0];
+        }
+        
+        /// <summary>
+        /// Create a <see cref="Host"/>.
+        /// </summary>
+        /// <param name="instanceName">The name of the Seq instance, or null.</param>
+        /// <param name="baseUri">The base URI that the server can be accessed on.</param>
+        public Host(string baseUri, string instanceName)
+        {
+            BaseUri = baseUri ?? throw new ArgumentNullException(nameof(baseUri));
+            InstanceName = instanceName;
+
+            #pragma warning disable 612, 618 // Obsolete
+            ListenUris = new [] { baseUri };
+            #pragma warning restore 612, 618            
         }
 
         /// <summary>
@@ -24,6 +48,12 @@
         /// <summary>
         /// The URIs at which the server is listening.
         /// </summary>
+        [Obsolete("Use BaseUri.")]
         public string[] ListenUris { get; }
+
+        /// <summary>
+        /// The base URI that the server can be accessed on.
+        /// </summary>
+        public string BaseUri { get; }
     }
 }
